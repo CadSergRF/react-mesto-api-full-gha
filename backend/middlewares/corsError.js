a755afa780363caf475b4609429913f5047de2f2
Sergey Evdokimov <cadsergrf@yandex.ru>
2023-05-13 18:26:04 +0700

    cors

--- backend/middlewares/corsError.js
+++ backend/middlewares/corsError.js
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
